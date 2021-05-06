100602cf4d9cb9d1ba4cf0d5eaff9e8a5f13ada0
Alan Razo Peña <alanrazop@MacBook-Air-de-Alan.local>
2021-05-06 18:32:33 -0500

    Iniciando ciclos

--- convolucion_ysobel.py
+++ convolucion_ysobel.py
@@ -16,3 +16,21 @@ def convolucion (img, kernel):
     m = math.floor((np.shape(kernel)[0] - 1) / 2)
     n = math.floor((np.shape(kernel)[1] - 1) / 2)
     
+    "Matriz resultante"
+    final = np.zeros((int(np.shape(mat)[0]), int(np.shape(mat)[1])), dtype = int)
+
+    "Inicializacion de ciclos"
+    num1 = 0
+    for i in range (np.shape(mat)[0] - np.shape(kernel)[0] + 1):
+        for j in range (np.shape(mat)[1] - np.shape(kernel)[1] + 1):
+            "Se obtiene la matriz del producto punto"
+            num1 += np.multiply((mat[i:i + (np.shape(kernel)[0]),
+                                     j:j + (np.shape(kernel)[1])]), kernel)
+            "Se coloca la suma de estos valores en la matriz resultante"
+            final[i + m][j + n] = num1.sum()
+            "Se repite el proceso asignando el cero a la valor de la variable"
+            num1 = 0
+    "Matriz resultante"
+    plt.imshow(final, cmap='gray')
+    plt.title("Imagen usando Kernel")
+    plt.show()
