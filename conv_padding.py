7aa9cccfc67ab6c3515184c71020d5ba9ec4d101
Hilda Beltrán <hildabeltran@Hildas-MacBook-Pro.local>
2021-05-07 09:46:55 -0500

    Eliminar ciclo while

--- conv_padding.py
+++ conv_padding.py
@@ -4,8 +4,6 @@ import math
 import cv2
 import matplotlib.pyplot as plt
 
-#Filtro = [[-1 0 1]; [-2 0 2]; [-1 0 1]] y edge
-
 def convolucion (img, kernel):
     #Convierte ambas entradas a matrices con ayuda de numpy
     mat = np.matrix(img)
@@ -15,10 +13,10 @@ def convolucion (img, kernel):
     n = math.floor((np.shape(kernel)[1] - 1) / 2)
     
     #Crea la matriz resultante, con valores de cero
-    final = np.zeros((int(np.shape(mat)[0]), int(np.shape(mat)[1])), dtype = int)
+    final = np.zeros((int(np.shape(mat)[0]), int(np.shape(mat)[1])),
+                     dtype = int)
 
     #Se inicializan ambos ciclos for
-    
     num1 = 0
     for i in range (np.shape(mat)[0] - np.shape(kernel)[0] + 1):
         for j in range (np.shape(mat)[1] - np.shape(kernel)[1] + 1):
@@ -27,30 +25,28 @@ def convolucion (img, kernel):
                                      j:j + (np.shape(kernel)[1])]), kernel)
             #Se coloca la suma de estos valores en la matriz resultante
             final[i + m][j + n] = num1.sum()
-            #Se le asigna el valor de cero a la variable, para repetir el proceso
+            #Se le asigna el valor de cero a la variable,
+            #para repetir el proceso
             num1 = 0
     #Se imprime la matriz resultante
     plt.imshow(final, cmap='gray')
     plt.title("Imagen usando Kernel con Padding")
     plt.show()
 
+#Se piden los datos de la imagen y el filtro al usuario
 print("1. Sobel edge horizontal  2. Sobel edge vertical   3. Edge detection")
-opcion=input("Elige filtro: ")
+opcion = input("Elige filtro: ")
 imagen = input('Nombre del archivo: ')
 img = cv2.imread(imagen, cv2.IMREAD_GRAYSCALE)
 
-while True:
-    if opcion=="1":
-        kernel = '[[-1 -2 -1]; [0 0 0]; [1 2 1]]'
-        break
+if opcion=="1":
+    kernel = '[[-1 -2 -1]; [0 0 0]; [1 2 1]]'
     
-    elif opcion=="2":
-        kernel= '[[-1 0 1]; [-2 0 2]; [-1 0 1]]'
-        break
+elif opcion == "2":
+    kernel= '[[-1 0 1]; [-2 0 2]; [-1 0 1]]'
         
-    else:
-        kernel = '[[-1 -1 -1]; [-1 8 -1]; [-1 -1 -1]]'
-        break
+elif opcion == "3":
+    kernel = '[[-1 -1 -1]; [-1 8 -1]; [-1 -1 -1]]'
     
 #Llama a la función, mandando ambas matrices
 convolucion(img, kernel)
